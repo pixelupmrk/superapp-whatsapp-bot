2973bb60d552abfe6f3141fa8521a72509c6cbd2
pixelupmrk <pixelupmrkt@gmail.com>
2025-12-21 11:10:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,45 +4,44 @@ const qrcode = require("qrcode");
 const app = express();
 const port = 8080;
 
-let sessions = {}; // Armazena as conexões ativas
-let qrCodes = {};  // Armazena os QRs por ID
+let qrCodes = {};
+let sessions = {};
 
-async function startWhatsApp(id) {
-    if (sessions[id]) return; // Já está rodando
-
-    const { state, saveCreds } = await useMultiFileAuthState('sessions/' + id);
+async function connectToWhatsApp(userId) {
+    if (sessions[userId]) return;
+    const { state, saveCreds } = await useMultiFileAuthState('sessions/' + userId);
     const sock = makeWASocket({ auth: state, printQRInTerminal: true });
-    sessions[id] = sock;
+    sessions[userId] = sock;
 
     sock.ev.on('creds.update', saveCreds);
     sock.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect, qr } = update;
-        if (qr) qrCodes[id] = await qrcode.toDataURL(qr);
+        if (qr) qrCodes[userId] = await qrcode.toDataURL(qr);
         if (connection === 'open') {
-            qrCodes[id] = "CONECTADO";
-            console.log(`Cliente ${id} conectado!`);
+            qrCodes[userId] = "CONECTADO";
+            console.log(`[${userId}] Conectado com sucesso!`);
         }
         if (connection === 'close') {
-            delete sessions[id];
+            delete sessions[userId];
             const shouldReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
-            if (shouldReconnect) startWhatsApp(id);
+            if (shouldReconnect) connectToWhatsApp(userId);
         }
     });
 }
 
-app.get("/qrcode", (req, res) => {
-    const id = req.query.id || 'default';
-    if (!sessions[id]) startWhatsApp(id);
-
+app.get("/qrcode", async (req, res) => {
+    const userId = req.query.id || 'default';
+    if (!sessions[userId]) connectToWhatsApp(userId);
+    
     setTimeout(() => {
-        const status = qrCodes[id];
-        if (status === "CONECTADO") res.send(`<h1>${id} está ON!</h1>`);
-        else if (status) res.send(`<img src="${status}" width="300"><p>ID: ${id}</p>`);
-        else res.send("<h1>Gerando QR... aguarde e atualize.</h1>");
-    }, 3000);
+        const status = qrCodes[userId];
+        if (status === "CONECTADO") res.send(`<h1>${userId} está Conectado!</h1>`);
+        else if (status) res.send(`<img src="${status}" style="width:300px;"><p>ID: ${userId}</p>`);
+        else res.send("<h1>Gerando QR... atualize a página.</h1>");
+    }, 2000);
 });
 
-// ESCUTA EM 0.0.0.0 PARA PERMITIR ACESSO EXTERNO
-app.listen(port, '0.0.0.0', () => { 
-    console.log("Servidor Multiusuário ONLINE na porta " + port); 
+// FORÇANDO ESCUTA EXTERNA
+app.listen(port, '0.0.0.0', () => {
+    console.log(`SERVIDOR MULTIUSUÁRIO ONLINE NA PORTA ${port}`);
 });
